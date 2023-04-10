61667a7dedaf2a30364539d1649bda3c14e06ced
Vin R <vramroop6@gmail.com>
2023-04-10 10:48:30 -0400

    fix orentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import {BsFillMoonStarsFill} from "react-icons/bs";
+import { BsLightbulb } from "react-icons/bs";
 import {AiFillTwitterCircle, AiFillMail, AiFillGithub} from "react-icons/ai";
 import Image from "next/image";
 import deved from "public/pfp.jpg";
@@ -31,14 +32,11 @@ export default function Home() {
             <h1 className="text-xl dark:text-white">Vinesh</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={() => setdarkMode(!darkMode)} className="cursor-pointer text-2xl dark:fill-white"/>
+                <BsLightbulb onClick={() => setdarkMode(!darkMode)} className="cursor-pointer text-2xl dark:fill-white"/>
                 </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" 
-                href="#"
-                >
-                  Resume
-                  </a>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"  
+                href="#">Resume</a>
                 </li>
             </ul>
           </nav>
@@ -49,7 +47,7 @@ export default function Home() {
               A person who likes programming and learning new things.
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 text-black">
+          <div className="text-5xl flex justify-center gap-16 mt-2 mb-5 text-black">
             <a href="https://twitter.com/Vintheruler1R" target="_blank">
               <AiFillTwitterCircle className="dark:fill-white"/>
             </a> 
@@ -62,13 +60,13 @@ export default function Home() {
               <AiFillGithub className="dark:fill-white"/>
             </a>
           </div>
-          <div className="relative mx-auto my-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden">
+          <div className="relative mx-auto my-auto mt-10 bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden">
             <Image src={deved} layout="fill" objectFit="cover"/>
           </div>
         </section>
         
         <section>
-          <div>
+          <div className="mt-8">
             <h3 className="text-3xl py-4 dark:text-white">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-black dark:text-white">
               I offer a wide range of services such as <span className="text-teal-500"> Discord Bots</span>,<span className="text-teal-500"> Websites </span>, and <span className="text-teal-500">AI/Machine Learning</span>.
@@ -76,40 +74,41 @@ export default function Home() {
           </div>
 
           <div className="lg: flex gap-2">
-            <div className="text-center shadow-lg p-9 rounded-xl my-10 dark:bg-gray-500">
-              <Image src={code} width={100} height={100}/>
+          <div className="text-center shadow-lg p-9 rounded-xl my-10 dark:bg-gradient-to-r from-blue-400 to-blue-600">
+              <Image src={code} width={120} height={120}/>
               <h3 className = "text-lg font-medium pt-8 pb-2">Discord Bots</h3>
-              <p className = "py-2">
-                Creating usable and customizable Discord Bots to suite your needs.
+              <p className = "py-2 flex-wrap text-center">
+                Highly customizable and reliable Discord Bots for your Discord server.
               </p>
               <h4 className="text-lg py-4 text-teal-600">Tools I use</h4>
-              <p className="text-black py-1">VS Code</p>
               <p className="text-black py-1">Python</p>
               <p className="text-black py-1">Nextcord</p>
+              <p className="text-black py-1">Lightbulb Hosting</p>
             </div>
 
-            <div className="text-center shadow-lg p-9 rounded-xl my-10 dark:bg-gray-500">
-              <Image src={design} width={100} height={100}/>
+            <div className="text-center shadow-lg p-9 rounded-xl my-10 dark:bg-gradient-to-r from-blue-600 to-purple-400">
+              <Image src={design} width={120} height={120}/>
               <h3 className = "text-lg font-medium pt-8 pb-2">Websites</h3>
-              <p className = "py-2">
-                Modern and simple websites that display information.
+              <p className = "py-2 flex-wrap text-center">
+                Modern and simple websites that display information for companies or individuals.
               </p>
               <h4 className="text-lg py-4 text-teal-600">Tools I use</h4>
-              <p className="text-black py-1">VS Code</p>
               <p className="text-black py-1">React.js</p>
               <p className="text-black py-1">Tailwind CSS</p>
+              <p className="text-black py-1">Vercel/Github</p>
             </div>
 
-            <div className="text-center shadow-lg p-9 rounded-xl my-10 dark:bg-gray-500">
-              <Image src={consulting} width={100} height={100}/>
+            <div className="text-center shadow-lg p-9 rounded-xl my-10 dark:bg-gradient-to-r from-purple-400 to-purple-700">
+              <Image src={consulting} width={120} height={120}/>
               <h3 className = "text-lg font-medium pt-8 pb-2">AI/Machine Learning</h3>
-              <p className = "py-2">
-                AI/Machine Learning algorithms ready to be deployed.
+              <p className = "py-2 flex-wrap text-center">
+                AI/Machine Learning algorithms ready to be deployed in your projects.
               </p>
               <h4 className="text-lg py-4 text-teal-600">Tools I use</h4>
-              <p className="text-black py-1">VS Code</p>
               <p className="text-black py-1">Python</p>
               <p className="text-black py-1">Tensorflow</p>
+
+              <p classname="text-sm text-black"></p>
             </div>
           </div>
 
@@ -117,13 +116,7 @@ export default function Home() {
 
         <section>
         <div>
-          <h3 className="text-3xl py-1">Portofolio</h3>
-          <p className="text-md py-2 leading-8 text-black dark:text-white">
-              I offer a wide range of services such as <span className="text-teal-500"> Discord Bots</span>,<span className="text-teal-500"> Websites </span>, and <span className="text-teal-500">AI/Machine Learning</span>.
-          </p>
-          <p className="text-md py-2 leading-8 text-black dark:text-white">
-          FillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFiller
-          </p>
+          <h3 className="text-3xl py-1">Some of my work</h3>
         </div>
         <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
           <div className="basis-1/3 flex-1">
@@ -134,15 +127,6 @@ export default function Home() {
           </div>
           <div className="basis-1/3 flex-1">
             <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={"100%"} layout="responsive"/>
-          </div>
-          <div className="basis-1/3 flex-1">
-            <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={"100%"} layout="responsive"/>
-          </div>
-          <div className="basis-1/3 flex-1">
-            <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={"100%"} layout="responsive"/>
-          </div>
-          <div className="basis-1/3 flex-1">
-            <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={"100%"} layout="responsive"/>
           </div>
         </div>
         </section>
